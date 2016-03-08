190d0b82d85f5e55618c6cc1ec21ce17084f8968
patsaindon <pat.saindon@gmail.com>
2016-03-08 14:24:40 -0500

    Update GetDisabledExpired.ps1

--- ADCleanup/GetDisabledExpired.ps1
+++ ADCleanup/GetDisabledExpired.ps1
@@ -5,10 +5,10 @@ $ou = "DC=siq,DC=local"
 Search-ADAccount -AccountExpired | Format-Table Name, DistinguishedName, AccountExpirationDate -AutoSize | Out-File "C:\Scripts\ADCleanup\ComptesSuspendusDateExpiration.txt" -enc utf8            
 
 ##Assembles and sends completion email with DL information##
-$emailFrom = "cous02@sqi.gouv.qc.ca"
-$emailTo = "cous02@sqi.gouv.qc.ca"
+$emailFrom = "email@company.com"
+$emailTo = "email@company.com"
+$smtpServer = "mailserver.company.local"
 $subject = "Rapports comptes utilisateurs avec date d'expiration"
-$smtpServer = "fermexcv01-pro.siq.local"
 $attachment1 = "C:\Scripts\ADCleanup\ComptesSuspendusDateExpiration.txt"
 
 
